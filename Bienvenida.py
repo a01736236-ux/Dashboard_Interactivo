f41cd699086a42e30ecdfb690869ae93da8cf101
a01736236-ux <a01736236@tec.mx>
2025-10-01 18:17:30 -0600

    Creacion de funciones back

--- Bienvenida.py
+++ Bienvenida.py
@@ -9,3 +9,23 @@ def suma():
     print("Suma total: ", a + b + c)
 
 suma()
+
+def generar_datos_ventas():
+    dias = ["Lun", "Mar", "Mie", "Jue", "Vie"]
+    ventas = [120, 500, 230, 289, 310]
+    return dias, ventas
+
+def resumen_analitica():
+    dias, ventas = generar_datos_ventas()
+    total = sum(ventas)
+    promedio = total / len(ventas)
+    return {
+        "dias": dias, 
+        "ventas": ventas,
+        "total": total, 
+        "promedio": promedio
+    }
+
+datos = resumen_analitica()
+print("Total de ventas :", datos ["total"])
+print("Promedio diario:", datos ["promedio"])
