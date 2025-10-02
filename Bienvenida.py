ee0d2495243a41d015b6268d6d947cad171465f7
a01736236-ux <a01736236@tec.mx>
2025-10-01 18:44:31 -0600

    Creacion de funciones del front

--- Bienvenida.py
+++ Bienvenida.py
@@ -9,3 +9,22 @@ def suma():
     print("Suma total: ", a + b + c)
 
 suma()
+
+import matplotlib.pyplot as plt
+
+def graficar_ventas(dias, ventas):
+    plt.figure(figsize=(8,4))
+    plt.plot(dias, ventas, marker = "o", linestyle ="-", color ="red")
+    plt.title("Ventas diarias")
+    plt.xlabel("Día")
+    plt.ylabel("Ventas")
+    plt.grid(True)
+    
+    plt.show()
+
+def main_frontend(): 
+    dias = ["Lun", "Mar", "Mie", "Jue", "Vie"]
+    ventas = [120, 500, 230, 289, 310]
+    graficar_ventas(dias, ventas)
+
+main_frontend()
